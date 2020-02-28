881ea39a64790a8aaa1b89a8e2d3ca182c371110
ali jafari <ali.jafari20@gmail.com>
2020-02-28 06:45:43 +0330

    shingling + minhashing + minhashfunctions added

---
+++ LSH.py
@@ -0,0 +1,58 @@
+import numpy as np
+from PermutationHashFunctions import *
+
+def shingler(Document= None , k = 1):
+    if Document is None or k < 1:
+        return None
+    else:
+        res = set()
+        for i in range(len(Document)-k+1):
+            res.add(Document[i:i+k])
+        return res
+
+HASHFUNCTIONS = [H1 , H2]
+
+def MinHasher(*sets):
+    """
+    returns signature of all given binary `sets`.
+    `sets` should be binary lists, and equal in length.
+
+    Ex.
+    set1 = [1,0,0,1,0,1,1,0,0]
+    set2 = [0,1,1,1,0,1,1,1,1]
+    set3 = [1,0,1,0,1,0,1,0,1]
+    for i,j in enumerate(MinHasher(set1, set2, set3)):
+        print('set{}'.format(i), j)
+    """
+    try:
+        t = type(sets[0])
+        l = len(sets[0])
+    except:
+        return None
+    for i in sets:
+        try:
+            if type(i) != t or len(i) != l:
+                return None
+        except:
+            return None
+    lineNumber = [x for x in range(l)]
+    Signature = [l for x in range(len(HASHFUNCTIONS))]
+    Signatures = [Signature for x in range(len(sets))]
+    Signatures = np.array(Signatures)
+    for i in lineNumber:
+        PermutedOrder = []
+        for h in HASHFUNCTIONS:
+            PermutedOrder.append(h(i))
+        for j,s in enumerate(sets):
+            if s[i] == 1:
+                for x in range(len(HASHFUNCTIONS)):
+                    if Signatures[j,x] > PermutedOrder[x]:
+                        Signatures[j,x] = PermutedOrder[x]
+
+    return Signatures
+
+a = [1,0,1,1,0]
+b = [0,1,1,0,1]
+
+for i,j in enumerate(MinHasher(a,b)):
+    print('set{}'.format(i), j)
---
+++ PermutationHashFunctions.py
@@ -0,0 +1,22 @@
+def H1(x):
+    return (x+1)%5
+
+def H2(x):
+    return ((2*(x+1))+1)%5
+
+##TEST 1
+# def H1(x):
+#     return [1,2,3,4,5,6,7]
+#
+# def H2(x):
+#     return [7,6,5,4,3,2,1]
+#
+# def H3(x):
+#     return [6,3,1,7,2,5,4]
+
+##TEST 2
+# def H1(x):
+#     return (x+1)%5
+#
+# def H2(x):
+#     return ((2*(x+1))+1)%5
