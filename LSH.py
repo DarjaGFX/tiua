c445f7a35deb725a47170bd81b87e474c11c9d90
ali jafari <ali.jafari20@gmail.com>
2020-03-13 02:21:06 +0330

    timer bugfix

--- LSH.py
+++ LSH.py
@@ -1,6 +1,8 @@
 import numpy as np
 from PermutationHashFunctions import *
 
+HASHFUNCTIONS = [H1 , H2]
+
 def shingler(Document= None , k = 1):
     if Document is None or k < 1:
         return None
@@ -9,8 +11,6 @@ def shingler(Document= None , k = 1):
         for i in range(len(Document)-k+1):
             res.add(Document[i:i+k])
         return res
-
-HASHFUNCTIONS = [H1 , H2]
 
 def MinHasher(*sets):
     """
@@ -35,7 +35,7 @@ def MinHasher(*sets):
                 return None
         except:
             return None
-    lineNumber = [x for x in range(l)]
+    lineNumber = list(range(l))
     Signature = [l for x in range(len(HASHFUNCTIONS))]
     Signatures = [Signature for x in range(len(sets))]
     Signatures = np.array(Signatures)
@@ -50,6 +50,7 @@ def MinHasher(*sets):
                         Signatures[j,x] = PermutedOrder[x]
 
     return Signatures
+
 
 a = [1,0,1,1,0]
 b = [0,1,1,0,1]
--- timer.py
+++ timer.py
@@ -13,12 +13,46 @@ def end():
     END = datetime.utcnow()
     print(END)
 
+def s2d(seconds):
+    SECOND = int(seconds % 60)
+    MINUTE = int((seconds//60) % 60)
+    HOUR = int((seconds//3600) % 24)
+    DAY = int((seconds//86400))
+    return DAY, HOUR, MINUTE, SECOND
+
 def printlog():
+    """
+    use start()
+    use end()
+    use printlog()
+    """
     DURATION = END - START
     print(END)
     s = DURATION.seconds
-    SECOND = s % 60
-    MINUTE = (s//60) % 60
-    HOUR = (s//3600) % 24
-    DAY = (s//86400)
+    DAY, HOUR, MINUTE, SECOND = s2d(s)
     print('it takes you {}:{}:{}\':{}" to run this script.'.format(DAY, HOUR, MINUTE, SECOND))
+
+_total = 0
+step = 0
+def tick():
+    global step
+    step += 1
+
+def loading():
+    """
+    set `_total`
+    use start()
+    use tick()
+    use loading()
+    """
+    percent = step*100/_total
+    now = datetime.utcnow()
+    passed = now - START
+    passed = passed.seconds
+    remaining = (passed*100//percent) - passed
+    DAY, HOUR, MINUTE, SECOND = s2d(remaining)
+    ETA = '{}:{}:{}:{}'.format(DAY, HOUR, MINUTE, SECOND)
+    DAY, HOUR, MINUTE, SECOND = s2d(passed)
+    ET = '{}:{}:{}:{}'.format(DAY, HOUR, MINUTE, SECOND)
+    print('\r {}'.format(' '*120), end='\r')
+    print('\r{} of {} | {}% \tET:{} \tETA:{} {}'.format(step, _total, percent, ET, ETA, ' '*10), end='\r')
